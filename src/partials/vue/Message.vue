b64d246c2e723fbba22f86f7f64100d516b4890a
noliaki <noliaki.mobile@gmail.com>
2017-09-12 22:47:06 +0900

    :+1: add tweet button

--- src/partials/vue/Message.vue
+++ src/partials/vue/Message.vue
@@ -2,10 +2,14 @@
 .speech_wrap(ref="wrap")
   transition-group.speech-lists(tag="ul", name="list", @enter="enter", ref="lists")
     li.speech-list(v-for="(item, index) in messages", key="index")
-      .speech-list--date
-        | {{ item.date }}
-      .speech-list--text
-        | {{ item.message }}
+      .speech-list--left_col
+        .speech-list--date
+          | {{ item.date }}
+        .speech-list--text
+          | {{ item.message }}
+      .speech-list--right_col
+        a.tweet(href="#tweet", @click.prevent="tweet(index)")
+          i.fa.fa-twitter-square(aria-hidden="true")
 </template>
 <script>
 export default {
@@ -18,6 +22,15 @@ export default {
   methods: {
     enter () {
       this.$refs.wrap.scrollTop = this.$refs.lists.$el.clientHeight
+    },
+    tweet (index) {
+      const shareURL = encodeURIComponent(this.messages[index].message)
+
+      window.open(
+        `https://twitter.com/intent/tweet?text=${shareURL}`,
+        '',
+        'width=550, height=450, personalbar=0, toolbar=0, scrollbars=1, resizable=!'
+      )
     }
   }
 }
